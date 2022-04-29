e495d4efa291d6c5cc40a598b99f62f704df3462
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-29 14:25:14 +0900

    Initial Commit

---
+++ Shared/MVVMPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMPracticeApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/29.
+//
+
+import SwiftUI
+
+@main
+struct MVVMPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
